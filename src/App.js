67d7b3d36d36c05c9d82f8b52b0992f07687c0ae
houssemhannachi <houssem.hannachi@enis.tn>
2021-11-13 19:17:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import NotFound  from "./pages/NotFound";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home/>}/>
+        <Route path="/a-propos" exact element={<About/>}/>
+        <Route path="*" exact={true} element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
